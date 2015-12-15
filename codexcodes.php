14009ec9bc7ca47163fbaf4f0ee1bddf2b927a41
bristweb <jonathan@bristleconeweb.com>
2015-12-14 21:40:45 -0500

    Create codexcodes.php

---
+++ codexcodes.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name:        CodexCodes
+Plugin URI:         https://github.com/bristweb/codexcodes
+Description:        Deeply integrated shortcodes for WordPress
+Version:            .1
+Author:             Bristlecone Web
+Author URI:         https://bristleconeweb.com/
+
+License:            GNU GPLv2
+License URI:        https://github.com/bristweb/codexcodes/blob/master/LICENSE
+*/
+
+namespace CodexCodes;
